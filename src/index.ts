faba521f2c924d99d69bbfe5010aa8b03a4565a5
Sebastian Wieland <sebasti@nwie.land>
2020-06-19 15:40:50 +0200

    Added help message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('Hello World');
+import program from 'commander';
+
+program
+    .name('documentarchiver')
+    .version('0.1.0')
+    .description('Propertly archive physical documents digitally');
+
+program.parse(process.argv);
