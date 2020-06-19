dd38d999eaa346b2e7a22f68fc9f7fba2f308d13
Sebastian Wieland <sebasti@nwie.land>
2020-06-19 15:54:50 +0200

    Paper sizes

---
+++ src/Constants.ts
@@ -0,0 +1,49 @@
+export class Constants {
+    public static paperFormats: Record<string, PaperFormat> = {
+        a0: {
+            name: 'a0',
+            width: 841,
+            height: 1189,
+        },
+        a1: {
+            name: 'a1',
+            width: 594,
+            height: 841,
+        },
+        a2: {
+            name: 'a2',
+            width: 420,
+            height: 594,
+        },
+        a3: {
+            name: 'a3',
+            width: 297,
+            height: 420,
+        },
+        a4: {
+            name: 'a4',
+            width: 210,
+            height: 297,
+        },
+        a5: {
+            name: 'a5',
+            width: 148,
+            height: 210,
+        },
+        a6: {
+            name: 'a6',
+            width: 105,
+            height: 148,
+        },
+        a7: {
+            name: 'a7',
+            width: 74,
+            height: 105,
+        },
+        a8: {
+            name: 'a8',
+            width: 52,
+            height: 74,
+        },
+    };
+}
---
+++ src/interfaces/PaperFormat.ts
@@ -0,0 +1,16 @@
+interface PaperFormat {
+    /**
+     * The name of this paper format.
+     */
+    name: string;
+
+    /**
+     * The width of this format in millimetres
+     */
+    width: number;
+
+    /**
+     * The height of this format in millimetres
+     */
+    height: number;
+}
